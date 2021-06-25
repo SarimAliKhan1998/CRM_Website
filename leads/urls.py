b807bfd6fa68aa015d146532eef44fdeafe86728
Sarim <sarimshujaat@gmail.com>
2021-06-25 17:54:58 +0530

    Created LeadForm using forms.Form

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import lead_detail_view, lead_list_view
+from .views import lead_create_view, lead_detail_view, lead_list_view
 
 app_name = "leads"
 
 urlpatterns = [
         path('', lead_list_view),
-        path("<int:pk>/", lead_detail_view)
+        path("<int:pk>/", lead_detail_view),
+        path("create/", lead_create_view)
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm
 
 # Create your views here.
 
@@ -23,3 +24,29 @@ def lead_detail_view(request, pk):
         "object": obj,
     }
     return render(request, "leads/lead_detail.html", context)
+
+
+
+def lead_create_view(request):
+
+    form = LeadForm()
+
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name_field = form.cleaned_data["first_name"]
+            last_name_field = form.cleaned_data["last_name"]
+            age_field = form.cleaned_data["age"]
+            agent_field = Agent.objects.first()
+
+            Lead.objects.create(
+                                first_name = first_name_field,
+                                 last_name = last_name_field,
+                                 age = age_field,
+                                 agent = agent_field
+                                 )
+            return redirect("/leads")
+    context = {
+        "form" : form
+    }
+    return render(request, "leads/lead_create.html", context)
